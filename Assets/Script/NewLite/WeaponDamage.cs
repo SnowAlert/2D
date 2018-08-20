8ede83f5c389a3ef6e22ca6228a9b19131a2e214
SnowAlert <goodbox0@gmail.com>
2018-08-20 22:32:01 +1100

    formating-script

--- Assets/Script/NewLite/WeaponDamage.cs
+++ Assets/Script/NewLite/WeaponDamage.cs
@@ -1,118 +1,113 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections;
 
 public class WeaponDamage : NetworkBehaviour
 {
-	public int Damage = 1;
-	public float attackTime = 1.0f;
-	// Время пока оружие наносит урон при контакте
-	public AnimationClip[] anim;
-	//[SyncVar]
-	//public int NumberAnim = 1;
-	[SyncVar]
-	public int BuferAnim = 4;
-	[SyncVar]
-	private float attackTimeConstant;
-	[SyncVar]
-	private bool startTime = false;
-	[SyncVar]
-	private bool nowHit = false;
-	[SyncVar]
-	private bool attackCompleted = true;
-	public GameObject player;
-	//public NetworkIdentity net;
+    public int Damage = 1;
+    public float attackTime = 1.0f;
+    // Время пока оружие наносит урон при контакте
+    public AnimationClip[] anim;
+    //public int NumberAnim = 1;
+    [SyncVar]
+    public int BuferAnim = 3;
+    [SyncVar]
+    private float attackTimeConstant;
+    [SyncVar]
+    private bool startTime = false;
+    [SyncVar]
+    private bool nowHit = false;
+    [SyncVar]
+    private bool attackCompleted = true;
+    public GameObject player;
 
-	void OnTriggerStay2D (Collider2D coll)
-	{
-		if (coll.name != "Player(Clone)") {
-			if (coll.gameObject.tag == "Player")
-			if (startTime == true)
-			if (nowHit == false)
-			if (attackCompleted == false) {
-				GameObject whoHit = coll.gameObject;
-				CmdHit (whoHit, Damage);
-				nowHit = true;
-			}
-		}
-	}
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.name != "Player(Clone)")
+        {
+            if (coll.gameObject.tag == "Player")
+                if (startTime == true)
+                    if (nowHit == false)
+                        if (attackCompleted == false)
+                        {
+                            GameObject whoHit = coll.gameObject;
+                            CmdHit(whoHit, Damage);
+                            nowHit = true;
+                        }
+        }
+    }
 
-	[Command]
-	void CmdHit (GameObject hit, int _damdge)
-	{
-		Hit (hit,_damdge);
-	}
-	void Hit(GameObject hit, int _damdge){
-		Health health = hit.GetComponent<Health> ();
-		if (health != null) {
-			health.TakeDamage (_damdge);
-			print (hit.name + " health: " + health.currentHealth);
-		}
-	}
+    [Command]
+    void CmdHit(GameObject hit, int _damdge)
+    {
+        Hit(hit, _damdge);
+    }
+    void Hit(GameObject hit, int _damdge)
+    {
+        Health health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(_damdge);
+            print(hit.name + " health: " + health.currentHealth);
+        }
+    }
 
-	void Start ()
-	{
-		player = transform.parent.gameObject;
-		player = player.transform.parent.gameObject;
-		attackTimeConstant = attackTime;
-		//player = transform.Find ("Player Test(Clone)").gameObject;
-		//this.transform.parent = transform.Find ("Player Test(Clone)");
+    void Start()
+    {
+        player = transform.parent.gameObject;
+        player = player.transform.parent.gameObject;
+        attackTimeConstant = attackTime;
+    }
 
-	}
-
-
-	void Update ()
-	{
-		
-		//if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			if (!player.GetComponent<NetworkIdentity> ().isServer) {
-				
-				print ("Local");
-				CmdWeaponAnim ();
-			}
+    void Update()
+    {
+        //if (Input.GetKeyDown (KeyCode.Mouse0)) {
+        if (!player.GetComponent<NetworkIdentity>().isServer)
+        {
+            print("Local");
+            CmdWeaponAnim();
+        }
         else
         {
             print("!Local");
             ClientWeaponAnim();
         }
-				
-		//}
-		if (startTime == true) {
-			attackTime -= Time.deltaTime;
-		}
-		if (attackTime <= 0) {
-
-			startTime = false;
-			attackTime = attackTimeConstant; //возвращаем переменной задержки её первоначальное значение из константы
-			nowHit = false;
-		}
-	}
-
-	[Command]
-	void CmdWeaponAnim ()
-	{
-		ClientWeaponAnim ();
-	}
-
-	
-	void ClientWeaponAnim ()
-	{
-        
-        if ((Input.GetButtonDown("Fire1")) && (attackTime >= attackTimeConstant))
+        //}
+        if (startTime == true)
+        {
+            attackTime -= Time.deltaTime;
+        }
+        if (attackTime <= 0)
         {
 
+            startTime = false;
+            attackTime = attackTimeConstant; //возвращаем переменной задержки её первоначальное значение из константы
+            nowHit = false;
+        }
+    }
+
+    [Command]
+    void CmdWeaponAnim()
+    {
+        ClientWeaponAnim();
+    }
+
+    void ClientWeaponAnim()
+    {
+        if ((Input.GetButtonDown("Fire1")) && (attackTime >= attackTimeConstant))
+        {
             if (BuferAnim == 1)
             {
                 print("BuferAnim=1 hit Right");
                 GetComponent<Animation>().Play(anim[0].name);
             }
-            if (BuferAnim == 2) { 
+            if (BuferAnim == 2)
+            {
                 print("BuferAnim=2 hit Left");
                 GetComponent<Animation>().Play(anim[1].name);
             }
-			startTime = true; 
-			attackCompleted = false;
-		}
+            startTime = true;
+            attackCompleted = false;
+        }
         if (BuferAnim == 3 && startTime == false)
         {
             GetComponent<Animation>().Play(anim[2].name);
@@ -123,6 +118,5 @@ public class WeaponDamage : NetworkBehaviour
             GetComponent<Animation>().Play(anim[3].name);
             print("BuferAnim=4 Stand");
         }
-
     }
 }
